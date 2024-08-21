8b824116d82b25a081fccde125545e9e809ad359
Destiny <dejenkins@justworks.com>
2024-08-21 13:42:22 -0400

    added comments to connection.js file to help navigate

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
+// create imports for express and mongoose
 const { connect, connection } = require('mongoose');
 
+// create connection to MongoDB
 const connectionString = 'mongodb://127.0.0.1:27017/api-socailNetworkdb';
 
+// connect to MongoDB
 connect(connectionString);
 
+// export connection
 module.exports = connection;
